8d2fdcac1b3c5cfd928c54516f3e2a55887e188e
horan MA <horans@gmail.com>
2018-08-10 21:08:24 +0800

    Update resmerge.bat

--- resmerge.bat
+++ resmerge.bat
@@ -4,19 +4,19 @@ setlocal enabledelayedexpansion
 ::config
 set m[1]o=_vendor.js
 set m[1]l=.\vendor\
-set m[1]f=es6-promise.auto.min.js lodash.min.js vue.min.js vuex.min.js vue-router.min.js md5.min.js uuid.core.js element-ui.min.js jquery.min.js
+set m[1]f=vendor1.min.js vendor2.min.js vendor3.min.js vendor4.min.js vendor5.min.js vendor6.min.js
  
 set m[2]o=_vendor.css
 set m[2]l=.\vendor\
-set m[2]f=element-ui-reset.css element-ui.min.css
+set m[2]f=vendor1.min.css vendor2.min.css vendor3.min.css
  
 set m[3]o=_inpage.js
 set m[3]l=.\script\
-set m[3]f=wsid-auth-l10n.js wsid-auth-excs.js wsid-auth.js
+set m[3]f=a.js b.js c.js d.js e.js
  
 set m[4]o=_inpage.css
 set m[4]l=.\style\
-set m[4]f=bootstrap-ao.css wsid-icon.css wsid-auth.css
+set m[4]f=a.css b.css c.css
 
 ::loop
 for /l %%n in (1,1,4) do (
